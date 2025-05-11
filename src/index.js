87442da76019b47d6fae797aec93647f076bc303
Seohoo_Jang <jjs2961@naver.com>
2025-05-11 15:23:44 +0900

    6

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter basename="/godnf">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </QueryClientProvider>
